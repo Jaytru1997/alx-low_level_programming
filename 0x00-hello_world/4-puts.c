8a3d0be9b7a7e8e3603b9aa1d0695a45aee7fd43
Jaytru1997 <kparaboj@gmail.com>
2021-06-17 07:50:08 -0700

    c program to output a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*
+prints "Programming is like building a multilingual puzzle"
+*/
+int main(){
+    puts("\"Programming is like building  a multilingual puzzle");
+    return (0);
+}
