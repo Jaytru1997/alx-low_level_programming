db0a9edab846f38259d76ce2b0acd3fb30db3278
Jaytru1997 <kparaboj@gmail.com>
2021-06-18 06:36:05 -0700

    print combo betwee 00 adn 99

---
+++ 0x01-variables_if_else_while/100-print_combo3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints out all the possible numbers between 00 and 99 
+ * without printing same digits
+ * Return: 0
+ */
+int main(void)
+{
+int i, j;
+
+for (i = 48; i < 58; i++)
+{
+for (j = i; j < 58; j++)
+{
+putchar(i);
+putchar(j);
+if (i == 57 && j == 57)
+{
+break;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
