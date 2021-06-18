3ad42a811461762217ea93f93ee8c5a2f68675f0
Jaytru1997 <kparaboj@gmail.com>
2021-06-18 06:08:06 -0700

    print out numbersseparated by commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints out all the numbers under 10 with a ,
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+if (i != 57)
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
