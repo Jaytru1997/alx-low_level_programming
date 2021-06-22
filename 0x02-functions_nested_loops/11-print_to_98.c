2dc6f7ba8339af9f91969304d97c34e1dcc8d4d6
Jaytru1997 <kparaboj@gmail.com>
2021-06-22 08:47:15 -0700

    from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_to_98 - prints every number from n to 98
+ * @n: starting number
+ */
+void print_to_98(int n)
+{
+int i;
+
+if (n <= 98)
+{
+for (i = n; i < 98; i++)
+{
+printf("%i ", i);
+}
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+printf("%i ", i);
+}
+}
+printf("98\n");
+}
