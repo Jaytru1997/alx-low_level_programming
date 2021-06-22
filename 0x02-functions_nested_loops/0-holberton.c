d9190fcd88573555f1f1d131d43a29f68e0f15fc
Jaytru1997 <kparaboj@gmail.com>
2021-06-22 00:33:24 -0700

    read me file and 0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - prints Holberton
+ * Return: 0
+ */
+int main(void)
+{
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+return (0);
+}
