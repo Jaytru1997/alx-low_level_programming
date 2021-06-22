92322a63e59b8acef8fd5a400f32295a6e76ea44
Jaytru1997 <kparaboj@gmail.com>
2021-06-22 02:25:44 -0700

    pritn alphabets 10 times

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include <holberton.h>
+#include "holberton.h"
 /**
  * main - prints Holberton
  * Return: 0
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char a;
+
+for (i = 0; i < 10; i++)
+{
+for (a = 'a'; c <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
+}
