3708fa9de40f91d3aeaa5a0fa44604901512016d
Jaytru1997 <kparaboj@gmail.com>
2021-06-22 08:26:32 -0700

    addition of to numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - adds parameters together
+ * @i: first integer
+ * @j: second integer
+ * Return: returns i + j
+ */
+int add(int i, int j)
+{
+return (i + j);
+}
