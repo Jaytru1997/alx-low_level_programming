b49118a220dac19837940d6b762ef55aaf273c90
Jaytru1997 <kparaboj@gmail.com>
2021-06-22 00:49:31 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <holberton.h>
+/**
+ * main - prints alphabets
+ * Return: 0
+ */
+int main(void)
+{
+char a;
+
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+return (0);
+}
