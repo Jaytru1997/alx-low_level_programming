2c109b9f1f8686e008f1b885b55b208125b808ac
Jaytru1997 <kparaboj@gmail.com>
2021-06-24 05:48:44 -0700

    modified 3-print_remaining_days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,7 +24,7 @@ else
 {
 if (month == 2 && day == 60)
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day -31, year);
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
