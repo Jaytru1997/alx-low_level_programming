5b20e0e5ccab61a0975a613222d6ce46b5c0a23e
Jaytru1997 <kparaboj@gmail.com>
2021-06-24 10:22:58 -0700

    print diagonally

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_diagonal - print a diagonal line of  arg length
+ * @n: length of line
+ */
+ovoid print_diagonal(int n)
+{
+int i = 0; j;
+
+if (n > 0)
+{
+while (i < n)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+i++;
+}
+}
+else
+{
+_putchar('\n');
+}
+}
