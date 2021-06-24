13885fa91c27e2b1dd87310c0d2659239a9cc891
Jaytru1997 <kparaboj@gmail.com>
2021-06-24 06:14:52 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _isupeer - checks if input is an uppercase letter in ASCI
+ * @c: integer
+ * Return: 1 if true, 0 if false
+ */
+int _isupper(int c)
+{
+return (c >= 65 && c <= 90);
+}
