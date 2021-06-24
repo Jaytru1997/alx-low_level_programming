bf7caf40e78c46d3185c66e9f4d8500e0d3a647d
Jaytru1997 <kparaboj@gmail.com>
2021-06-24 03:28:21 -0700

    modified 0-main.c and 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,7 @@ int main(void)
 {
 int i;
 
-printf("Infinite loop incoming:(\n");
+printf("Infinite loop incoming :(\n");
 i = 0;
 /*
  * while (i < 10)
@@ -15,6 +15,6 @@ i = 0;
  * putchar(i);
  * }
  */
-printf("Infinite loop avoided!\\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
