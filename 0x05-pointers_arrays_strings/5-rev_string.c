0535aa7e36af2b247c0761f1e09f60ef5f8ce466
Jaytru1997 <kparaboj@gmail.com>
2021-06-28 07:07:05 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*s)
+{
+s++;
+length++;
+}
+return (length);
+}
+/**
+ * rev_string - prints the reverse of a string
+ * @: string
+ */
+void rev_string(char *s)
+{
+int length = _strlen(s);
+int counter = 0;
+char tmp;
+
+while (counter < length / 2)
+{
+tmp = *(s + counter);
+*(s + counter) = *(s + length - 1 - counter);
+*(s + length - 1 - counter) = tmp;
+counter++;
+}
+}
