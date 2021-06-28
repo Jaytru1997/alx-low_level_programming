4ef37d460f5eb0f190ad12d17a88126933850e8b
Jaytru1997 <kparaboj@gmail.com>
2021-06-28 06:06:53 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*s)
+{
+length += 1;
+s += 1;
+}
+return (length);
+}
