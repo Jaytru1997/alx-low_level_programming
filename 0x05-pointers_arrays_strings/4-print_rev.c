c60b9f0745f7ac9c6edea177a7d9193781803908
Jaytru1997 <kparaboj@gmail.com>
2021-06-28 06:41:46 -0700

    print reverse and update holberton.h

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ *@s: string
+ *Return: length of string
+ */
+int _strlen(char *s)
+{
+char *p = s;
+
+while (*s)
+{
+s++;
+}
+return (s-p);
+}
+/**
+ * print_rev - prints the reverse of a string
+ * @s: string
+ */
+void print_rev(char *s)
+{
+int length = _strlen(s);
+
+while (length--)
+{
+_putchar(*(s + length));
+}
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -11,4 +11,14 @@ int _strlen(char *s);
 
 void _puts(char *str);
 
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
 #endif
