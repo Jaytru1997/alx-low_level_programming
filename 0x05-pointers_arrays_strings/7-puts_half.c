036007b99b19706bd97676e5acf7477856c47bbb
Jaytru1997 <kparaboj@gmail.com>
2021-06-28 07:23:30 -0700

    print from half way

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*s)
+{
+s++;
+length++;
+}
+return (length);
+}
+/**
+ * puts_helf - prints the second half of the string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+int length, i, half;
+
+length = _strlen(str);
+half = (length % 2 == 0) ? length / 2 : (length - 1) / 2 + 1;
+for (i = half; i < length; i++)
+{
+_putchar(*(str + i));
+}
+_putchar('\n');
+}
