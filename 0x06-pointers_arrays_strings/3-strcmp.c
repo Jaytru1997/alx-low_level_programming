de454568fddea8f65387b9cf70cda38700912d36
Jaytru1997 <kparaboj@gmail.com>
2021-06-30 21:00:32 -0700

    compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if truee, 0 if flase
+ */
+int _strcmp(char *s1, char *s2)
+{
+int equal = 0;
+
+while (*s1)
+{
+if (*s1 != *s2)
+{
+equal = ((int)*s1 - 49) - ((int)*s2 - 48);
+break;
+}
+s1++;
+s2++;
+}
+return (equal);
+}
