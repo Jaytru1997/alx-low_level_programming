9df34fe08b378fa05bd04d89587decf80e3cf805
Jaytru1997 <kparaboj@gmail.com>
2021-07-04 21:43:43 -0700

    set pointer value

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: double pointer to be copied
+ * @to: where s gets set to
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
