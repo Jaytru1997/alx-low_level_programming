dc4aa00233e019a09d2082d4ec14ebe560890fb5
Jaytru1997 <kparaboj@gmail.com>
2021-07-06 18:55:59 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * isSqrt - helper function for _sqrt_recursion
+ * @n: input number
+ * @i: iteratotr
+ * Return: square root of number
+ */
+
+int isSqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (isSqrt(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: input number
+ * Return: square root of number, -1 if false
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (isSqrt(n, 1));
+}
