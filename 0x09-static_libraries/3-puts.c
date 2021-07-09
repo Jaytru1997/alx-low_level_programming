8d9f69e048cf2b7c94edf31d441052ee83023450
Jaytru1997 <kparaboj@gmail.com>
2021-07-09 02:40:51 -0700

    required files

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
