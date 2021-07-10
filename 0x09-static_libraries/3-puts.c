7e7f766c5b8d9b34f6707e1acc3fa5feb20644c0
Jaytru1997 <kparaboj@gmail.com>
2021-07-10 00:10:48 -0700

    modified code base

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,6 +1,15 @@
 #include "holberton.h"
+
 /**
- * _puts - prints a string followed by  new line to stdout
- * @str: string to be rinted
+ * _puts - prints a string followed by a new line to stdout
+ * @str: string to be printed
  */
-void _puts(char *str);
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
