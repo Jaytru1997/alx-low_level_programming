305ef6fa0858bc1eea5d83ccd0ac1b7e5d9e82a3
Jaytru1997 <kparaboj@gmail.com>
2021-07-10 01:03:01 -0700

    modified code base

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -4,12 +4,12 @@
  * _puts - prints a string followed by a new line to stdout
  * @str: string to be printed
  */
-void _puts(char *str)
+void _puts(char *s)
 {
-	while (*str)
+	while (*s)
 	{
-		_putchar(*str);
-		str++;
+		_putchar(*s);
+		s++;
 	}
 	_putchar('\n');
 }
