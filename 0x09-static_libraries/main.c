11110cd79376f819a93624eeb9066f719e60125c
Jaytru1997 <kparaboj@gmail.com>
2021-07-10 10:35:29 -0700

    new files

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * main - prints string
+ * Return: Always 0
+ */
+int main(void)
+{
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
+}
