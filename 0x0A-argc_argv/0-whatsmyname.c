b2024649624a67b610d522429567f6d7d6a114ec
Jaytru1997 <kparaboj@gmail.com>
2021-07-12 05:12:14 -0700

    whats ,y
    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *  * main - prints its name, followed by a new line
+ *   * @argc: argument count
+ *    * @argv: argument vector
+ *     * Return: 0
+ *      */
+int main(int argc, char *argv[])
+{
+		(void)argc;
+			printf("%s\n", argv[0]);
+				return (0);
+}
