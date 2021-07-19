22608bc7f2235cb83f9af9dd0e67388f7cf2b89c
Jaytru1997 <kparaboj@gmail.com>
2021-07-19 07:53:08 -0700

    dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,13 +18,13 @@ char *name;
 float age;
 char *owner;
 }
-my_dog;
+dog_t;
 
 int _strlen(char *s);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-my_dog *new_dog(char *name, float age, char *owner);
-void free_dog(my_dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strdup(char *str);
 
 #endif
