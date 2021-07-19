90a3d9a6481d81c0ae33c87566b01e4f8da33487
Jaytru1997 <kparaboj@gmail.com>
2021-07-19 07:15:44 -0700

    modified dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,4 +19,11 @@ float age;
 char *owner; 
 }my_dog;
 
+int _strlen(char *s);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strdup(char *str);
+
 #endif
