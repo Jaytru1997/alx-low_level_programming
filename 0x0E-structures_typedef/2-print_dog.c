ccd09a70d77bf14b6d9a5dc11543acc06c9184de
Jaytru1997 <kparaboj@gmail.com>
2021-07-19 07:48:30 -0700

    print struct elements

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,8 +8,8 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
-printf("Name: %s\n", d->name ? d->name : ("nil"));
+printf("Name: %s\n", d->name ? d->name : "(nil)");
 printf("Age: %d\n", d->age);
-printf("Owner: %s\n", d->owner ? d->owner : ("nil"));
+printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
 }
