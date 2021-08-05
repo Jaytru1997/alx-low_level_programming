de700cf88f723cc690751bcc86bff1da4599e9e2
Jaytru1997 <kparaboj@gmail.com>
2021-08-05 11:46:13 -0700

    holberton header file and 0-binary_to_uint.c file

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,21 @@
+fndef HOLBERTON_H
+#define HOLBERTON_H
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+ 
+#define  BIT_SIZE 8
+ 
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+int _strlen(const char *s);
+_Bool isBinary(char);
+ 
+#endif /* HOLBERTON_H */
