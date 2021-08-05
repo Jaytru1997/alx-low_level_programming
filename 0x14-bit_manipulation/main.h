3962d7d475178955bd98a909a6ede0d34b6ac4d3
Jaytru1997 <kparaboj@gmail.com>
2021-08-05 12:13:42 -0700

    modified files

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
- 
+
 #define  BIT_SIZE 8
- 
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -17,5 +17,5 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 int _strlen(const char *s);
 _Bool isBinary(char);
- 
+
 #endif /* MAIN_H */
