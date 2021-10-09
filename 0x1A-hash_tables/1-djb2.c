fd7fc9fbcdfe7ebb2718396f5e738b7e2a26ffb3
Jaytru1997 <kparaboj@gmail.com>
2021-10-08 17:06:39 -0700

    djb2 algo

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,9 +1,8 @@
-#include "hash_tables.h"
-
 /**
- * hash_djb2 - the djb2 algorithm
- * @str: input string
- * Return: index of hash generated
+ * hash_djb2 - Computes the hash of an array of bytes using the db2 algorithm.
+ * @str: The byte array.
+ *
+ * Return: A fixed size unsigned integer digest.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
